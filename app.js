709c5272f6b984f78bf4772a32394e2c78be1165
MikailSeremet <mikail.seremet@gmail.com>
2017-06-13 23:42:48 +0000

    REST NEW and CREATE pages done

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.get("/", function(req, res) {
     res.redirect("/blogs")
 })
 
+// INDEX ROUTE
+
 app.get("/blogs",function(req,res){
     Blog.find({},function(err,blogs){
         if(err){
@@ -42,6 +44,25 @@ app.get("/blogs",function(req,res){
             res.render("index",{blogs:blogs});
         }
     });
+});
+
+// NEW ROUTE
+
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+// CREATE ROITE
+app.post("/blogs",function(req,res){
+   //create blog
+   Blog.create(req.body.blog,function(err,newBlog){
+       if(err){
+           res.render("new");
+       }else {
+            //redirect to index
+            res.redirect("/blogs");
+       }
+   });
 });
 
 app.listen(process.env.PORT,process.env.IP,function(){
